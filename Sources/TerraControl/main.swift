a25b962b54aeba7e21dd7e27664041e682a66d6d
Thomas Bonk <thomas@meandmymac.de>
2021-01-30 10:07:58 +0100

    refactored the main service

--- Sources/TerraControl/main.swift
+++ Sources/TerraControl/main.swift
@@ -20,29 +20,6 @@
 
 import Foundation
 import Commander
-import TerraControlCore
-import Pushover
-import Swifter
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
-
-var ctrl: TerraController? = nil
-var pushover: Pushover? = nil
-var pushoverUserKey: String = ""
-
-let port:UInt16 = 0xCAFE
-
-let ipcServer: HttpServer = {
-  let server = HttpServer()
-
-  server["/stop"] = { request in
-    ctrl?.stop()
-    return .ok(.htmlBody("OK"))
-  }
-
-  return server
-}()
 
 let main = command(
   Option<String>(
@@ -54,51 +31,12 @@ let main = command(
     default: false,
     description: "Stop a running TerraController instance")) { (configurationFile: String, stop: Bool) in
 
-  do {
-    guard !stop else {
-      let url = URL(string: "http://127.0.0.1:\(port)/stop")!
-      var request = URLRequest(url: url)
-      request.httpMethod = "GET"
-
-      let data = try NSURLConnection.sendSynchronousRequest(request, returning: nil)
-      print("\(data)")
-      return
-    }
-
-    let configUrl = URL(fileURLWithPath: configurationFile)
-    let data = try Data(contentsOf: configUrl)
-    let decoder = JSONDecoder()
-    let config = try decoder.decode(TerraControlConfiguration.self, from: data)
-
-    if let pushoverToken = config.pushoverToken {
-      pushover = Pushover(token: pushoverToken)
-      pushoverUserKey = config.pushoverUserKey!
-    }
-
-    let controller = try TerraController(configuration: config, pushover: pushover)
-    ctrl = controller
-
-    signal(SIGINT) { _ in ctrl!.stop() }
-    signal(SIGTERM) { _ in ctrl!.stop() }
-
-    try ipcServer.start(port, forceIPv4: true)
-    try controller.start()
-  } catch {
-    TerraControlLogger.error("Error while starting: \(error)")
-
-    if let push = pushover {
-      push
-        .sendNotification(
-          "A TerraControl error occurred: \(error)",
-          to: [pushoverUserKey],
-          title: "Error",
-          priority: .emergency,
-          sound: .spacealarm) { result in
-
-          TerraControlLogger.error("Result when sending notification: \(result)")
-        }
-    }
+  guard !stop else {
+    Service.shared.stopService()
+    return
   }
+
+  Service.shared.startService(configurationFile: configurationFile)
 }
 
 main.run()
