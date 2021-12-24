aeaf2f4f182b55a1f316611293dffcba048c22ce
Thomas Bonk <thomas@meandmymac.de>
2021-12-24 13:16:10 +0100

    scheduling programs for the same day

--- Sources/TerraControlCore/TerraController.swift
+++ Sources/TerraControlCore/TerraController.swift
@@ -23,175 +23,180 @@ import HAP
 import Logging
 
 public class TerraController: DeviceDelegate {
+  
+  // MARK: - Private Properties
+  
+  private let logger = Logger(class: TerraController.self)
+  private let configuration: Configuration
+  private let stateFile: String
+  private let location: Solar.Location
+  
+  private var keepRunning: Bool = true
+  
+  private var programScheduler: Scheduler!
+  private var switchEvents = [Scheduler]()
+  
+  private let revision = Revision("1")
+  
+  private var bridge: Device!
+  private var server: Server!
+  private var switches = [String:Accessory.Switch]()
+  
+  
+  // MARK: - Initialization
+  
+  public init(configuration: Configuration, stateFile: String) throws {
+    self.configuration = configuration
+    self.stateFile = stateFile
+    self.location =
+    Solar.Location(latitude: configuration.location.latitude, longitude: configuration.location.longitude)
     
-    // MARK: - Private Properties
-    
-    private let logger = Logger(class: TerraController.self)
-    private let configuration: Configuration
-    private let stateFile: String
-    private let location: Solar.Location
-    
-    private var keepRunning: Bool = true
-    
-    private var programScheduler: Scheduler!
-    private var switchEvents = [Scheduler]()
-    
-    private let revision = Revision("1")
-    
-    private var bridge: Device!
-    private var server: Server!
-    private var switches = [String:Accessory.Switch]()
-    
-    
-    // MARK: - Initialization
-    
-    public init(configuration: Configuration, stateFile: String) throws {
-        self.configuration = configuration
-        self.stateFile = stateFile
-        self.location =
-        Solar.Location(latitude: configuration.location.latitude, longitude: configuration.location.longitude)
-        
-        if let token = configuration.pushoverToken, let userKey = configuration.pushoverUserKey {
-            PushoverClient.shared.initialize(token: token, userKey: userKey)
-        }
-        
-        initializeSwitches()
-        try initializeBridge()
+    if let token = configuration.pushoverToken, let userKey = configuration.pushoverUserKey {
+      PushoverClient.shared.initialize(token: token, userKey: userKey)
     }
     
-    private func initializeSwitches() {
-        configuration
-            .terrariums
-            .forEach { terrarium in
-                switches =
-                switches
-                    .merging(
-                        terrarium
-                            .switches
-                            .reduce(into:  [String:Accessory.Switch]()) { dictionary, sw in
-                                dictionary[sw.id] =
-                                Accessory
-                                    .Switch(
-                                        info:
-                                            Service.Info(
-                                                name: "\(terrarium.name): \(sw.name)",
-                                                serialNumber: sw.serialNumber,
-                                                manufacturer: sw.manufacturer,
-                                                model: sw.model,
-                                                firmwareRevision: revision))
-                            },
-                        uniquingKeysWith: { (first, _) in first })
-            }
-    }
-    
-    private func initializeBridge() throws {
-        bridge =
-        Device(
-            bridgeInfo:
-                Service.Info(
-                    name: configuration.bridgeName,
-                    serialNumber: "de61442e-5b40-11ec-bf63-0242ac130002",
-                    manufacturer: "Thomas Bonk",
-                    model: "TerraController Bridge",
-                    firmwareRevision: revision),
-            setupCode: Device.SetupCode(stringLiteral: configuration.setupCode),
-            storage: FileStorage(filename: stateFile),
-            accessories: switches.values.shuffled())
-        server = try Server(device: bridge)
-        bridge.delegate = self
-    }
-    
-    
-    // MARK: - Start and stop
-    
-    public func start() throws {
-        logger.info("Starting TerraControl...")
-        PushoverClient.shared.information(message: "Starting TerraControl")
-        
-        keepRunning = true
-        
-        programScheduler =
-            Scheduler.schedule(
-                at: Date.tomorrow.startOfDay,
-                repeating: .seconds(24 * 60 * 60),
-                block: schedulePrograms)
-        
-        while keepRunning {
-            RunLoop.current.run(mode: .default, before: Date.distantFuture)
-        }
-        
-        logger.info("Stopping TerraControl...")
-        PushoverClient.shared.information(message: "Stopping TerraControl")
-        try server.stop()
-    }
-    
-    public func stop() {
-        DispatchQueue.main.async {
-            self.logger.info("Shutting down TerraController...")
-            
-            self.keepRunning = false
-        }
-    }
-    
-    
-    // MARK: - Private Methods
-    
-    private func schedulePrograms() {
-        logger.info("Scheduling programs...")
-        
-        switchEvents.removeAll()
-        
-        do {
-            let now = Date()
-            let noon = try Solar.noon(for: now, at: location, in: configuration.timezone!)
-            
-            configuration
-                .terrariums
-                .forEach { terrarium in
-                    let program = terrarium.currentProgram()
-                    
-                    PushoverClient.shared.information(message: "Scheduling program \(program!.name) for terrarium \(terrarium.name) at \(now.date(for: configuration.timezone!)). Noon is at \(noon.date(for: configuration.timezone!)).")
-                    
-                    program?
-                        .rules
-                        .forEach { rule in
-                            guard rule.hoursOn > 0 else {
-                                return
-                            }
-                            
-                            let halfIntervall =
-                              ((rule.hoursOn + rule.hoursOnIncrementPerDay * Double(now.day - terrarium.currentProgram()!.start)) / 2) * 3600
-                            
-                            
-                            guard halfIntervall > 0 else {
-                                return
-                            }
-                            
-                            let onTime = noon - halfIntervall
-                            let offTime = noon + halfIntervall
-                            
-                            switchEvents.append(Scheduler.schedule(at: onTime) {
-                                self.setState(switches: rule.switches, state: true)
-                            })
-                            switchEvents.append(Scheduler.schedule(at: offTime) {
-                                self.setState(switches: rule.switches, state: false)
-                            })
-                        }
-                }
-        } catch {
-            logger.error("Error while scheduling programs: \(error)")
-        }
-    }
-    
-    private func setState(switches: [String], state: Bool) {
+    initializeSwitches()
+    try initializeBridge()
+  }
+  
+  private func initializeSwitches() {
+    configuration
+      .terrariums
+      .forEach { terrarium in
+        switches =
         switches
-            .forEach { switchId in
-                let sw = self.switches[switchId]!
-                
-                sw.switch.powerState.value = state
-                
-                let stateDescription = state ? "ON" : "OFF"
-                PushoverClient.shared.information(message: "Setting power state of switch \(sw.info.name.value!) to \(stateDescription)")
-            }
+          .merging(
+            terrarium
+              .switches
+              .reduce(into:  [String:Accessory.Switch]()) { dictionary, sw in
+                dictionary[sw.id] =
+                Accessory
+                  .Switch(
+                    info:
+                      Service.Info(
+                        name: "\(terrarium.name): \(sw.name)",
+                        serialNumber: sw.serialNumber,
+                        manufacturer: sw.manufacturer,
+                        model: sw.model,
+                        firmwareRevision: revision))
+              },
+            uniquingKeysWith: { (first, _) in first })
+      }
+  }
+  
+  private func initializeBridge() throws {
+    bridge =
+    Device(
+      bridgeInfo:
+        Service.Info(
+          name: configuration.bridgeName,
+          serialNumber: "de61442e-5b40-11ec-bf63-0242ac130002",
+          manufacturer: "Thomas Bonk",
+          model: "TerraController Bridge",
+          firmwareRevision: revision),
+      setupCode: Device.SetupCode(stringLiteral: configuration.setupCode),
+      storage: FileStorage(filename: stateFile),
+      accessories: switches.values.shuffled())
+    server = try Server(device: bridge)
+    bridge.delegate = self
+  }
+  
+  
+  // MARK: - Start and stop
+  
+  public func start() throws {
+    logger.info("Starting TerraControl...")
+    PushoverClient.shared.information(message: "Starting TerraControl")
+    
+    keepRunning = true
+    
+    programScheduler =
+      Scheduler.schedule(
+        at: Date.tomorrow.startOfDay,
+        repeating: .seconds(24 * 60 * 60),
+        block: schedulePrograms)
+    
+    if Date.startOfDay < Date() {
+        schedulePrograms()
     }
+    
+    
+    while keepRunning {
+      RunLoop.current.run(mode: .default, before: Date.distantFuture)
+    }
+    
+    logger.info("Stopping TerraControl...")
+    PushoverClient.shared.information(message: "Stopping TerraControl")
+    try server.stop()
+  }
+  
+  public func stop() {
+    DispatchQueue.main.async {
+      self.logger.info("Shutting down TerraController...")
+      
+      self.keepRunning = false
+    }
+  }
+  
+  
+  // MARK: - Private Methods
+  
+  private func schedulePrograms() {
+    logger.info("Scheduling programs...")
+    
+    switchEvents.removeAll()
+    
+    do {
+      let now = Date()
+      let noon = try Solar.noon(for: now, at: location, in: configuration.timezone!)
+      
+      configuration
+        .terrariums
+        .forEach { terrarium in
+          let program = terrarium.currentProgram()
+          
+          PushoverClient.shared.information(message: "Scheduling program \(program!.name) for terrarium \(terrarium.name) at \(now.date(for: configuration.timezone!)). Noon is at \(noon.date(for: configuration.timezone!)).")
+          
+          program?
+            .rules
+            .forEach { rule in
+              guard rule.hoursOn > 0 else {
+                return
+              }
+              
+              let halfIntervall =
+              ((rule.hoursOn + rule.hoursOnIncrementPerDay * Double(now.day - terrarium.currentProgram()!.start)) / 2) * 3600
+              
+              
+              guard halfIntervall > 0 else {
+                return
+              }
+              
+              let onTime = noon - halfIntervall
+              let offTime = noon + halfIntervall
+              
+              switchEvents.append(Scheduler.schedule(at: onTime) {
+                self.setState(switches: rule.switches, state: true)
+              })
+              switchEvents.append(Scheduler.schedule(at: offTime) {
+                self.setState(switches: rule.switches, state: false)
+              })
+            }
+        }
+    } catch {
+      logger.error("Error while scheduling programs: \(error)")
+    }
+  }
+  
+  private func setState(switches: [String], state: Bool) {
+    switches
+      .forEach { switchId in
+        let sw = self.switches[switchId]!
+        
+        sw.switch.powerState.value = state
+        
+        let stateDescription = state ? "ON" : "OFF"
+        PushoverClient.shared.information(message: "Setting power state of switch \(sw.info.name.value!) to \(stateDescription)")
+      }
+  }
 }
